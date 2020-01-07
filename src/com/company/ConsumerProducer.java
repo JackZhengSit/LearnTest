fc96b9bf24fc2e594a70d829621a7cc141f22b04
JackZheng <JackZhengSit@outlook.com>
2020-01-07 15:01:42 +0800

    CyclicBarrierSample

---
+++ src/com/company/ConsumerProducer.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ConsumerProducer {
+
+    public static final String EXIT_MSG = "Good Bye!";
+
+    public static void main(String[] args) {
+        BlockingQueue<String> queue = new ArrayBlockingQueue<>(3);
+        Producer p=new Producer(queue);
+        Consumer c=new Consumer(queue);
+        new Thread(p).start();
+        new Thread(c).start();
+
+    }
+
+    static class Producer implements Runnable {
+        private BlockingQueue<String> queue;
+
+        public Producer(BlockingQueue<String> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 20; i++) {
+                try {
+                    Thread.sleep(5L);
+                    String msg = "Message" + i;
+                    System.out.println("Produced new item: " + i);
+                    queue.put(msg);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            try {
+                System.out.println("Time to say good bye!");
+                queue.put(EXIT_MSG);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    static class Consumer implements Runnable {
+        private BlockingQueue<String> queue;
+
+        public Consumer(BlockingQueue<String> queue) {
+            this.queue = queue;
+        }
+
+        @Override
+        public void run() {
+            try{
+                String msg;
+                while (!EXIT_MSG.equalsIgnoreCase((msg=queue.take()))){
+                    System.out.println("Consumer item:"+msg);
+                    Thread.sleep(10L);
+                }
+                System.out.println("Got exit message, Bye! ");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
